ea4dc42c5b95f83950b0d01a7edf1eaed6ec4997
Stanislav Rogovskyi <stanislav_rogovskyi@epam.com>
2016-11-04 18:09:25 +0200

    bucket event type fixed

--- src/main/java/com/nkttk/core/components/events/BucketEventType.java
+++ src/main/java/com/nkttk/core/components/events/BucketEventType.java
@@ -11,4 +11,14 @@ public enum BucketEventType {
   public String getS3Name(){
     return this.s3Name;
   }
+
+  BucketEventType byName(String name){
+    BucketEventType [] values = BucketEventType.values();
+    for(int i = 0; i < values.length; i++){
+      if(values[i].s3Name.equals(name)){
+        return values[i];
+      }
+    }
+    throw new RuntimeException("Bucket event type not : " + name);
+  }
 }
